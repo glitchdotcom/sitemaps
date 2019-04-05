5bf04679f006c26eef333f3996532cf8d6de1c42
Glitch (hello-express) <none>
2019-04-05 16:27:57 +0000

    💄🚎 Checkpoint ./generate.js:301412/7352

--- generate.js
+++ generate.js
@@ -1,20 +1,14 @@
 const algoliaSitemap = require('algolia-sitemap');
 const indices = require('./constants').INDICES;
 
-const algoliaConfig = {
-  appId: process.env.ALGOLIA_APP_ID,
-  apiKey: process.env.ALGOLIA_API_KEY,
-  indexName: indices.users,
-};
-
 const args = process.argv.slice(2) || indices;
 args.length ? generate(args) : generate();
 
 const glitchDomain = 'https://glitch.com';
 
-function generate(indices = ['projects', 'users', 'teams', 'collections']) {
+function generate(sections = ['projects', 'users', 'teams', 'collections']) {
   let locTemplate;
-  for (let index of indices) {
+  for (let index of sections) {
     switch (index) {
       case 'projects':
         locTemplate = (project) => `${glitchDomain}/~${project.domain}`;
@@ -31,20 +25,25 @@ function generate(indices = ['projects', 'users', 'teams', 'collections']) {
         break;
       */
     }
-    
+
+    const algoliaConfig = {
+      appId: process.env.ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_API_KEY,
+      indexName: indices[index],
+    };
+
     const hitToParams = (item) => {
       const loc = locTemplate(item);
-      console.log(loc);
-//       const lastmod = new Date().toISOString();
-//       const priority = 0.6; // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
+      const lastmod = new Date().toISOString();
+      const priority = 0.6; // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
 
-//       return {
-//         loc,
-//         lastmod,
-//         priority,
-//       };
+      return {
+        loc,
+        lastmod,
+        priority,
+      };
     };
-    
+
     // sitemaps must be <= 50k entries per file, and <= 50 MB
     // algolia-sitemap paginates automatically: sitemaps/sitemap.{n}.xml
     algoliaSitemap({

6f21907769498a7e08dc7ff325c5b21207b6bc47
Glitch (hello-express) <none>
2019-04-05 16:17:47 +0000

    🏚🚇 Checkpoint ./generate.js:301412/1517 ./.glitch-assets:301412/390 ./views/index.html:301412/189 ./README.md:301412/685

---
+++ README.md
@@ -0,0 +1,11 @@
+ # Sitemaps generator for glitch.com
+
+Open the console and run `npm run generate` to rebuild the sitemaps:
+
+```
+# rebuild all the sitemaps (projects, users, teams, collections)
+npm run generate
+
+# rebuild the projects sitemap
+npm run generate projects
+```
--- generate.js
+++ generate.js
@@ -7,27 +7,13 @@ const algoliaConfig = {
   indexName: indices.users,
 };
 
-const hitToParams = (item) => {
-  console.log(item);
-  const loc = locTemplate(item);
-    `https://glitch.com/@${item.login}`;
-  const lastmod = new Date().toISOString();
-  const priority = 0.6; // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
-
-  return {
-    loc,
-    lastmod,
-    priority,
-  };
-};
-
-const args = process.argv.slice(2);
+const args = process.argv.slice(2) || indices;
 args.length ? generate(args) : generate();
 
-let locTemplate;
 const glitchDomain = 'https://glitch.com';
 
 function generate(indices = ['projects', 'users', 'teams', 'collections']) {
+  let locTemplate;
   for (let index of indices) {
     switch (index) {
       case 'projects':
@@ -38,11 +24,26 @@ function generate(indices = ['projects', 'users', 'teams', 'collections']) {
         break;
       /* TODO: these are being re-indexed soon
       case 'teams':
-        locTemplate = (team) => `${glitchDoomain}/@${team.login}`;
+        locTemplate = (team) => `${glitchDomain}/@${team.login}`;
         break;
       case 'collections':
-        locTemplate = (team) => 
+        locTemplate = (team) => `${glitchDomain}/@${collection.fullUrl}`;
+        break;
+      */
     }
+    
+    const hitToParams = (item) => {
+      const loc = locTemplate(item);
+      console.log(loc);
+//       const lastmod = new Date().toISOString();
+//       const priority = 0.6; // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
+
+//       return {
+//         loc,
+//         lastmod,
+//         priority,
+//       };
+    };
     
     // sitemaps must be <= 50k entries per file, and <= 50 MB
     // algolia-sitemap paginates automatically: sitemaps/sitemap.{n}.xml

22e2af8b5d317d9bebfc39890debba44da127a60
Glitch (hello-express) <none>
2019-04-05 15:13:40 +0000

    👞🐋 Checkpoint ./generate.js:301412/2060 ./constants.js:301412/12

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 exports.INDICES = {
   collections: 'search_collections',
   teams: 'search_teams',
-  users: 'search_teams',
+  users: 'search_users',
   projects: 'search_projects',
 };
--- generate.js
+++ generate.js
@@ -4,14 +4,20 @@ const indices = require('./constants').INDICES;
 const algoliaConfig = {
   appId: process.env.ALGOLIA_APP_ID,
   apiKey: process.env.ALGOLIA_API_KEY,
-  indexName: indices.teams,
+  indexName: indices.users,
 };
 
 const hitToParams = (res) => {
-  // const url = // `https://${lang}.yoursite.com/${lang}/detail/${objectID}`;
-  // const loc = url({ lang: 'en', objectID });
+  console.log(res);
+  const loc = `https://glitch.com/@${res.login}`;
   const lastmod = new Date().toISOString();
   const priority = 0.6; // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
+
+  return {
+    loc,
+    lastmod,
+    priority,
+  };
 };
 
 // sitemaps must be <= 50k entries per file, and <= 50 MB

6eaf60740050a757d428a612c0372cc0e5d05a00
Glitch (hello-express) <none>
2019-04-05 14:51:44 +0000

    💳🎮 Checkpoint ./constants.js:301412/585 ./generate.js:301412/673 ./package.json:301412/427

--- constants.js
+++ constants.js
@@ -1,1 +1,6 @@
-export const 
+exports.INDICES = {
+  collections: 'search_collections',
+  teams: 'search_teams',
+  users: 'search_teams',
+  projects: 'search_projects',
+};
--- generate.js
+++ generate.js
@@ -1,1 +1,21 @@
-const algoliaSitemap = require('algolia-sitemap');
+const generate = require('algolia-sitemap');
+const indices = require('./constants').INDICES;
+
+const algoliaConfig = {
+  appId: process.env.ALGOLIA_APP_ID,
+  apiKey: process.env.ALGOLIA_API_KEY,
+  indexName: indices.teams,
+};
+
+const hitToParams = (res, i) => {
+  console.log(i);
+};
+
+// sitemaps must be <= 50k entries per file, and <= 50 MB
+// Algolia 
+generate({
+  algoliaConfig,
+  outputFolder: 'sitemaps',
+  hitToParams,
+});
+  

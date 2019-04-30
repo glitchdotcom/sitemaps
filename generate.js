3a0e376676ad8ac90dcdb574e722e8aafc1167db
Glitch (sitemaps) <none>
2019-04-30 15:03:41 +0000

    🐸🏄 Checkpoint ./generate.js:301412/5188 ./package.json:301412/1084

--- generate.js
+++ generate.js
@@ -1,5 +1,15 @@
 const algoliaSitemap = require('algolia-sitemap');
-const Gauge = require('gauge');
+const chalk = require('chalk');
+var ProgressBar = require('progress');
+ 
+var bar = new ProgressBar(':bar', { total: 10 });
+var timer = setInterval(function () {
+  bar.tick();
+  if (bar.complete) {
+    console.log('\ncomplete\n');
+    clearInterval(timer);
+  }
+}, 100);
 
 const indices = require('./constants').INDICES;
 
@@ -9,64 +19,60 @@ const args = process.argv.slice(2) || indices;
 args.length ? generate(args) : generate();
 
 function generate(sections = ['projects', 'users', 'teams', 'collections']) {
-  const gauge = new Gauge()
-  console.log(`Generating sitemaps for ${sections.join(', ')}`);
-  gauge.show("test", 0.20);
-  gauge.pulse("this");
-  gauge.hide();
-  
-//   let locTemplate;
-//   for (let index of sections) {
-//     switch (index) {
-//       case 'projects':
-//         locTemplate = (project) => `${glitchDomain}/~${project.domain}`;
-//         break;
-//       case 'users':
-//         locTemplate = (user) => `${glitchDomain}/@${user.login}`;
-//         break;
-//       case 'teams':
-//         locTemplate = (team) => `${glitchDomain}/@${team.url}`;
-//         break;
-//       case 'collections':
-//         locTemplate = (collection) => `${glitchDomain}/@${collection.fullUrl}`;
-//         break;
-//     }
+  console.log(chalk.blue.bold(`Generating sitemaps for ${sections.join(', ')}`));
 
-//     const algoliaConfig = {
-//       appId: process.env.ALGOLIA_APP_ID,
-//       apiKey: process.env.ALGOLIA_API_KEY,
-//       indexName: indices[index],
-//     };
+  //   let locTemplate;
+  //   for (let index of sections) {
+  //     switch (index) {
+  //       case 'projects':
+  //         locTemplate = (project) => `${glitchDomain}/~${project.domain}`;
+  //         break;
+  //       case 'users':
+  //         locTemplate = (user) => `${glitchDomain}/@${user.login}`;
+  //         break;
+  //       case 'teams':
+  //         locTemplate = (team) => `${glitchDomain}/@${team.url}`;
+  //         break;
+  //       case 'collections':
+  //         locTemplate = (collection) => `${glitchDomain}/@${collection.fullUrl}`;
+  //         break;
+  //     }
 
-//     const hitToParams = (item) => {
-//       // get template for formatting the full URL
-//       const loc = locTemplate(item);
+  //     const algoliaConfig = {
+  //       appId: process.env.ALGOLIA_APP_ID,
+  //       apiKey: process.env.ALGOLIA_API_KEY,
+  //       indexName: indices[index],
+  //     };
 
-//       // set lastmod with updatedAt if it's available, otherwise use the current date
-//       const date = item.updatedAt ? new Date(item.updatedAt) : new Date();
-//       const lastmod = date.toISOString();
+  //     const hitToParams = (item) => {
+  //       // get template for formatting the full URL
+  //       const loc = locTemplate(item);
 
-//       // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
-//       const priority = 0.6;
+  //       // set lastmod with updatedAt if it's available, otherwise use the current date
+  //       const date = item.updatedAt ? new Date(item.updatedAt) : new Date();
+  //       const lastmod = date.toISOString();
 
-//       if (item.notSafeForKids || item.isPrivate) {
-//         return null;
-//       }
+  //       // see discussion https://www.notion.so/glitch/Sitemaps-36446db005414f87af9910c51e21d88e#1a0eff53ae9c492aa9be33ceac1126b8
+  //       const priority = 0.6;
 
-//       return {
-//         loc,
-//         lastmod,
-//         priority,
-//       };
-//     };
+  //       if (item.notSafeForKids || item.isPrivate) {
+  //         return null;
+  //       }
 
-//     // sitemaps must be <= 50k entries per file, and <= 50 MB
-//     // algolia-sitemap paginates automatically
-//     algoliaSitemap({
-//       algoliaConfig,
-//       sitemapLoc: `${glitchDomain}/sitemaps/${index}`,
-//       outputFolder: `.data/${index}`,
-//       hitToParams,
-//     });
-  }
+  //       return {
+  //         loc,
+  //         lastmod,
+  //         priority,
+  //       };
+  //     };
+
+  //     // sitemaps must be <= 50k entries per file, and <= 50 MB
+  //     // algolia-sitemap paginates automatically
+  //     algoliaSitemap({
+  //       algoliaConfig,
+  //       sitemapLoc: `${glitchDomain}/sitemaps/${index}`,
+  //       outputFolder: `.data/${index}`,
+  //       hitToParams,
+  //     });
+  // }
 }

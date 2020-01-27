895b770fd64381bb117cb68b98edc17124c8bee5
Glitch (sitemaps) <none>
2020-01-27 20:56:42 +0000

    🚴🧀 Checkpoint ./api.js:9/36

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ module.exports.getUserById = async function(id) {
     const res = await axios.get(`https://api.glitch.com/v1/users/by/id?id=${id}`);
     return res.data;
   } catch(error) {
+    console.log(error);
     throw error;
   }
 };

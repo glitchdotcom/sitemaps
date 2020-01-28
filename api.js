864c57fecf29805c26c046aad672e6acb814ffea
Glitch (sitemaps) <none>
2020-01-28 16:32:01 +0000

    ✊📕 Checkpoint ./api.js:9/393 ./generate.js:9/39

--- api.js
+++ api.js
@@ -1,10 +1,19 @@
 const axios = require('axios');
-
-module.exports.getUserById = async function(id) {
+/*
+module.exports.getUserById = function(id) {
   try {
-    const res = await axios.get(`https://api.glitch.com/v1/users/by/id?id=${id}`);
+    const res = axios.get(`https://api.glitch.com/v1/users/by/id?id=${id}`);
+    console.log(res.data)
     return res.data;
   } catch(error) {
     console.log("error getting user " + id);
   }
+};
+*/
+
+module.exports.getUserById = function(id) {
+  return axios
+    .get(`https://api.glitch.com/v1/users/by/id?id=${id}`)
+    .then((res) => {res.data; console.log('api.js', res.data);})
+    .catch((error) => { console.log("error getting user " + id) });
 };

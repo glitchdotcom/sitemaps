1108c7bbd24cab995646c4dc22437c33c31c95d8
Glitch (sitemaps) <none>
2020-02-05 18:34:08 +0000

    🏞📊 Checkpoint ./generate.js:9/39

--- generate.js
+++ generate.js
@@ -73,7 +73,7 @@ async function generate(sections = ['projects', 'users', 'teams', 'collections']
       return atleastOneAuthedUser;
     };
 
-    const hitToParams = async (item) => {
+    const hitToParams = (item) => {
       // get template for formatting the full URL
       const loc = encodeURI(locTemplate(item));
 
@@ -90,12 +90,12 @@ async function generate(sections = ['projects', 'users', 'teams', 'collections']
       }
 
       // extra validation for projects: exclude anon and newly-created projects
-      if (index === 'projects' && await !isProjectValid(item)) {
+      if (index === 'projects' &&  !isProjectValid(item)) {
         return null;
       }
       
       // remove pages with a noindex tag: any users/teams/collections that are empty
-      if (index !== 'projects' && await isPageEmpty(item)) {
+      if (index !== 'projects' && isPageEmpty(item)) {
         return null;
       }
       

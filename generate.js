dc49aca092926cf6c68e1da344d008b3b8ffe54f
Glitch (sitemaps) <none>
2020-02-05 19:00:49 +0000

    📮👨‍👨‍👦 Checkpoint ./generate.js:9/106

--- generate.js
+++ generate.js
@@ -88,7 +88,7 @@ async function generate(sections = ['projects', 'users', 'teams', 'collections']
       if (item.notSafeForKids || item.isPrivate) {
         return null;
       }
-/*
+/* hitToParams doesn't play nicely with async functions, need more brains to figure out how to do this
       // extra validation for projects: exclude anon and newly-created projects
       if (index === 'projects' &&  !isProjectValid(item)) {
         return null;
